6d2430be3de90508169ffc6a937fc578082eca28
Dennis Lamm <expeditioneer@gentoo.org>
2025-01-19 19:53:28 +0100

    log.py reformatted file

--- DeComp/log.py
+++ DeComp/log.py
@@ -1,7 +1,7 @@
 # pylint:disable=invalid-name
 
 """
-Logging module for the Decomp libraries
+Logging module for the DeComp libraries
 This module can be overridden by the consumer application
 """
 
@@ -19,27 +19,31 @@ info = logger.info
 warning = logger.warning
 
 
-def set_logger(logpath: str = '', level: int = logging.DEBUG) -> None:
-    """Logger initialization function
+def set_logger(logfile_directory: str = '', level: int = logging.DEBUG) -> None:
+    """
+    Logger initialization function
 
-    :param logpath: optional filepath to save log output to
+    :param logfile_directory: optional filepath to save log output to
     :param level: logging level to set the file logger to
     """
     logger.setLevel(logging.INFO)
     # create formatter and add it to the handlers
     log_format = '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
     formatter = logging.Formatter(log_format)
+
     # add the handlers to logger
-    if logpath:
-        if not os.path.exists(logpath):
-            raise OSError(errno.ENOENT, f'Log file not found: {logpath}')
-        logname = os.path.join(logpath, f"DeComp-{time.strftime('%Y%m%d-%H:%M')}.log")
+    if logfile_directory:
+        if not os.path.exists(logfile_directory):
+            raise OSError(errno.ENOENT, f'Log file not found: {logfile_directory}')
+        logname = os.path.join(logfile_directory, f"DeComp-{time.strftime('%Y%m%d-%H:%M')}.log")
         file_handler = logging.FileHandler(logname)
         file_handler.setLevel(level)
         file_handler.setFormatter(formatter)
         logger.addHandler(file_handler)
+
     # create console handler with a higher log level
-    Console_handler = logging.StreamHandler()
-    Console_handler.setLevel(logging.ERROR)
-    # Console_handler.setFormatter(formatter)
-    logger.addHandler(Console_handler)
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.ERROR)
+
+    # console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
